f2614183e4c3a81e9837fc95cf5d8818bc14cfec
RaulHernandez23 <raulh230600@gmail.com>
2025-06-17 17:50:11 -0600

    agregado pais

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const usuarioRoutes = require('./routes/usuario.routes');
 const authRoutes = require('./routes/auth.routes');
 const paisRoutes = require('./routes/pais.routes');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/auth', authRoutes);
